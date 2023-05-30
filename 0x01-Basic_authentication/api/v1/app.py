0e1a2cac5557aa7b27ebd0757ca4eb37efdaaf42
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-30 18:25:57 +0300

    basic_auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from typing import Tuple
 
 
 app = Flask(__name__)
@@ -15,13 +16,13 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
-def not_found(error) -> str:
+def not_found(error) -> Tuple:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
+def unauthorized(error) -> Tuple:
     """ Unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
