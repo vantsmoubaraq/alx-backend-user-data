763ba3455ee4b33827768a608891b9cf42d4ada2
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-30 18:13:24 +0300

    basic_auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -24,7 +24,7 @@ def not_found(error) -> str:
 def unauthorized(error) -> str:
     """ Unauthorized
     """
-    return jsonify({{"error": "Unauthorized"}}), 401
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
