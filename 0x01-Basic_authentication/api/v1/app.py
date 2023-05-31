56c6d423645ced4f8f62db0e3fd158230d3676a1
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-31 07:20:25 +0300

    basic_auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,13 +22,13 @@ def not_found(error) -> Tuple:
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
-def unauthorized(error) -> Tuple:
+def unauthorized(error) -> str:
     """ Unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(403)
-def not_allowed(error) -> Tuple:
+def not_allowed(error) -> str:
     """
     Can't access resource
     """
