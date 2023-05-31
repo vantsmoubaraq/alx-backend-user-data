2c1f1f0df6ceab24c51d2e9136ce5d1da649f85d
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-31 08:52:53 +0300

    basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,3 +37,15 @@ class BasicAuth(Auth):
             return decoded_str
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """Extracts user credentials"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        credentials = decoded_base64_authorization_header.split(":")
+        return credentials[0], credentials[1]
