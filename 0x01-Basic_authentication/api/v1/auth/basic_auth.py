31d923f65f97062c3d2cdee6a8ea6e142a90e9a9
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-31 08:31:27 +0300

    basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,9 +7,19 @@ Module implements basic authentication features
 from flask import request
 from typing import List
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Class implements basic authentication features
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
+        """Encode in base64"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header.split(" ")[0] != "Basic":
+            return None
+        return authorization_header.split(" ")[1]
