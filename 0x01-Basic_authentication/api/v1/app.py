37f217433f31c82591cab9d6aef290a6a55dc962
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-31 08:15:13 +0300

    basic_auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,8 +20,12 @@ excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
 authenticated = getenv("AUTH_TYPE")
 
 if authenticated:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+    if authenticated == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    else:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Module implements basic authentication features
+"""
+
+from flask import request
+from typing import List
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Class implements basic authentication features
+    """
+    pass
