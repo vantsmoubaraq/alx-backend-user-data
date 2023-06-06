58fc0697f95228d7d525bf95eeb52a8f0409967c
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-06-06 11:49:26 +0300

    user_authentication

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -43,7 +43,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs: Any) -> User:
         """find user
         """
         try:
@@ -57,9 +57,12 @@ class DB:
     def update_user(self, user_id: str, **kwargs) -> None:
         """updates user
         """
-        user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            if not hasattr(user, key):
-                raise ValueError
-            setattr(user, key, value)
-        self._session.commit()
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if not hasattr(user, key):
+                    raise ValueError
+                setattr(user, key, value)
+            self._session.commit()
+        except Exception:
+            pass
