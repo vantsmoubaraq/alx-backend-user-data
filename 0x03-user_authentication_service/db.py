e65af6976470a4ecf94d1bdb8985a1245bb9a949
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-06-06 11:59:07 +0300

    user_authentication

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -54,13 +54,16 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: str, **kwargs: Any) -> None:
+    def update_user(self, user_id: str, **kwargs) -> None:
         """updates user
         """
-        user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            if not hasattr(user, key):
-                raise ValueError
-            else:
-                setattr(user, key, value)
-        self._session.commit()
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if not hasattr(user, key):
+                    raise ValueError
+                else:
+                    setattr(user, key, value)
+            self._session.commit()
+        except Exception:
+            pass
