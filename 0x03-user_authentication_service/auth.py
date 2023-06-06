cfc8b0260c0961cfeb6a6b6e147b3ab787af3a15
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-06-06 09:18:35 +0300

    user_authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,15 +12,3 @@ def _hash_password(password: str) -> bcrypt:
     """Encrypts password"""
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     return hashed_password
-
-
-class Auth:
-    """Auth class to interact with the authentication database.
-    """
-
-    def __init__(self):
-        self._db = DB()
-
-    def register_user(self, email: str, password: str) -> User:
-        """Registers new user if not exist"""
-        pass
