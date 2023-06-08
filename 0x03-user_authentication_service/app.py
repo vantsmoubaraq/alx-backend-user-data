d4f978c0ab0ecbaddea335ca3bfbc0775832c3e2
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-06-08 16:06:33 +0300

    user_authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,5 +74,19 @@ def profile():
         abort(403)
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """get reset token"""
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        if reset_token:
+            return jsonify({"email": email, "reset_token": reset_token})
+        else:
+            abort(403)
+    except Exception:
+        pass
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
